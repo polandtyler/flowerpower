dc924de43347586ae029ba7e6080a519eaee3873
Tyler Poland <poland.tyler@gmail.com>
2020-10-18 12:23:27 -0500

    Initial Commit

---
+++ FlowerPower/FlowerPowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerPowerApp.swift
+//  FlowerPower
+//
+//  Created by Tyler Poland on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct FlowerPowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
